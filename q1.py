05a925dabcab0316e1f6bfb5d7238fc7a2055d23
Nadav-Levintov <DSP155@Gmail.com>
2018-01-11 10:50:18 +0200

    Just the start...

--- q1.py
+++ q1.py
@@ -2,10 +2,11 @@ import csv
 
 import math
 import sklearn
+from sklearn import tree
 
 flare_list = []
 with open('flare.csv','r') as flare_file:
-    flare_reader = csv.reader(flare_file,delimiter=' ', quotechar='|')
+    flare_reader = csv.reader(flare_file,delimiter=',', quotechar='|')
     for row in flare_reader:
         flare_list.append(row)
 
@@ -14,3 +15,10 @@ flare_list.remove(flare_list[0])
 
 size =len(flare_list)
 folds = [flare_list[x:x + math.ceil(size/4)] for x in range(0, len(flare_list), math.ceil(size/4))]
+
+#X=[folds[0],features]
+#Y=folds[1]
+#clf = tree.DecisionTreeClassifier()
+#clf = clf.fit(X, Y)
+
+#clf.predict(folds[2])
